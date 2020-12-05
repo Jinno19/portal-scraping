eee02d782f666f5f06a6eca26c5384168efaded1
yuukari <c01171576b@edu.teu.ac.jp>
2020-12-05 13:08:33 +0900

    init

--- index2.js
+++ index2.js
@@ -1,33 +1,7 @@
-//import cron from 'node-cron';
-
 import { getNewInformations } from './getNewInfo.js';
 import { getLecturePage } from './getLectureInfo.js';
 import { puppeteerLauncher } from './get_CSreferenceinfo.js';
 
-/*
-//cron.schedule('0 10 * * * * ', () => {
-const newInfo = getNewInformations('https://service.cloud.teu.ac.jp/inside2/hachiouji/computer_science/');
-//});
-
-//cron.schedule('0 15 * * * * ', () => {
-const lectureInfo = getLecturePage('https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/');
-//});
-
-//cron.schedule('0 30 * * * * ', () => {
-const referenceInfo = puppeteerLauncher();
-//});
-*/
-
-
-
-
-/*
-const funcs = [
-    getNewInformations,
-    getLecturePage,
-    puppeteerLauncher,
-];
-*/
 
 (async () => {
     console.log('a');
