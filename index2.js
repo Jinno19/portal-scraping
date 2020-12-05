c7247601e5893aaf56a421dfdf5ba86e1e5d5dbc
yuukari <c01171576b@edu.teu.ac.jp>
2020-12-05 16:26:38 +0900

    init

--- index2.js
+++ index2.js
@@ -4,13 +4,8 @@ import { puppeteerLauncher } from './get_CSreferenceinfo.js';
 
 
 (async () => {
-    console.log('a');
     await getNewInformations('https://service.cloud.teu.ac.jp/inside2/hachiouji/computer_science/');
-    console.log('c');
+    await getLecturePage('https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/');
     await puppeteerLauncher();
 })();
 
-(async () => {
-    console.log('b');
-    await getLecturePage('https://service.cloud.teu.ac.jp/inside2/hachiouji/hachioji_common/cancel/');
-})();
